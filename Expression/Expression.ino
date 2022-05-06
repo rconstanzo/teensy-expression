dea8aa852a274a6dea468aad11b6405c3063e295
Rodrigo <rodrigo.constanzo@gmail.com>
2022-05-06 23:21:54 +0100

    typo

--- Expression/Expression.ino
+++ Expression/Expression.ino
@@ -77,11 +77,6 @@ Adafruit_ADS1115 ads;
 // variable to store sensor reading
 int16_t adc0;
 
-// flash LED on startup for troubleshooting purposes
-digitalWrite(led, HIGH);
-delay(500);
-digitalWrite(led, LOW);
-
 // initialize sensor reading
 ResponsiveAnalogRead analog(0, true);
 
@@ -120,6 +115,11 @@ void setup(void)
 
   // set smoothing resolution
   analog.setAnalogResolution(sensor_range);
+
+// flash LED on startup for troubleshooting purposes
+digitalWrite(led, HIGH);
+delay(500);
+digitalWrite(led, LOW);
 
   // reset sketch to initial state
   // EEPROM.write(10,255);
